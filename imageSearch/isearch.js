f39235d4e1843df6571306a1a1b1c0a7220bee1c
Meer Afzal <do4best@gmail.com>
2024-05-30 18:47:18 +0500

    we need to study more of such items

--- imageSearch/isearch.js
+++ imageSearch/isearch.js
@@ -14,19 +14,19 @@ inputData = searchInputEle.value; // first check the value of search input value
     console.log(url) // check ether url is working
     const response = await fetch(url) // response when api respond
     const data = await  response.json()// api response should be printed in json
-    if(page === 1){ //condation for if the page is one please respond nothing (i mean only one page)
-        searchResultEle.innerHTML = ""// display nothing
-    }
+    // if(page === 3){ //condation for if the page is one please respond nothing (i mean only one page)
+    //     searchResultEle.innerHTML = ""// display nothing
+    // }
     const results = data.results; // when api call response it should store in a result variable
     results.map((result)=>{ // then loop through it and extract the desired data
-        const imageWrapper = document.createElement("div")
-        imageWrapper.classList.add("search-result")
-        const image = document.createElement("img")
-        image.src = result.urls.small;
-        image.alt = result.alt_discription;
-        const imageLink = document.createElement("a");
-        imageLink.href = result.links.html;
-        imageLink.target = "_blank";
+        const imageWrapper = document.createElement("div") // create div and add it to Search Result
+        imageWrapper.classList.add("search-result") // add it search result
+        const image = document.createElement("img") // create an element for image
+        image.src = result.urls.small; // copy the image url as image src
+        image.alt = result.alt_discription;// and its description
+        const imageLink = document.createElement("a");// create an anchor tag
+        imageLink.href = result.links.html; // copy the resulted link
+        imageLink.target = "_blank"; // open it in another page if the link is clicked
         imageLink.textContent = result.alt_discription;
         imageWrapper.appendChild(image)
         imageWrapper.appendChild(imageLink)
@@ -42,10 +42,10 @@ if(page > 1){
 }
 }
 
-formEl.addEventListener("submit",(e)=>{
+formEl.addEventListener("submit",(e)=>{// all the code is executed upon pressing this button
     e.preventDefault()
     searchImage()
 
 })
 
-showmoreEle.addEventListener("click",()=>searchImage())
+showmoreEle.addEventListener("click",()=>searchImage()) // this code will be executed upon pressing browsing more.
